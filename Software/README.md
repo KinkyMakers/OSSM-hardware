f01481df1de8055560a11f363a2977b982f8d751
AJ <Ajay.Koenig@gmail.com>
2024-05-04 12:04:48 -0600

    fix: bring over other files

---
+++ Software/README.md
@@ -0,0 +1,37 @@
+# OSSM Software
+This is the recommended software for use with OSSM designs
+
+## Requirements:
+- Arduino IDE or PlatformIO
+- ESP32 microcontroller
+
+To use with Arduino, simply open the .ino file in the src directory. You will also need to copy the folders in the lib directory to your arduino library folder on your PC.
+
+Use the tutorial below to install the ESP32 board into your Arduino IDE so you're ready to program!
+
+[Setting up ESP32 on Arduino IDE](https://randomnerdtutorials.com/installing-the-esp32-board-in-arduino-ide-windows-instructions/)
+
+If you aren't sure what ESP board to select in Arduino IDE, choose "ESP32 Dev Module" - it works on all ESP32 I have tested so far.
+
+![ESP32 Architecture](https://github.com/KinkyMakers/OSSM-hardware/blob/master/PlatformIO%20ESP32%20code/OSSM_ESP32/OSSM%20ESP32%20Architecture.png)
+
+
+## Logging
+
+This project uses ESP32's [native logging library](https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/log.html).
+
+
+## Commands
+
+**Upload and monitor**
+```bash
+pio run -t upload -t monitor
+```
+
+**Run Unit Test tests**
+```bash
+pio test -e test
+```
+
+## Other Documentation
+- [Git Branching Strategy](docs/Git Branching Strategy)
---
+++ Software/src/utils/StateLogger.h
@@ -0,0 +1,64 @@
+#ifndef OSSM_SOFTWARE_STATELOGGER_H
+#define OSSM_SOFTWARE_STATELOGGER_H
+
+#include <Arduino.h>
+
+#include <cassert>
+
+#include "boost/sml.hpp"
+#include "constants/LogTags.h"
+
+namespace sml = boost::sml;
+using namespace sml;
+
+/**
+ * @brief Logs state machine events for the OSSM class.
+ *
+ * The StateLogger class is responsible for logging the events of the state
+ * machine used in the OSSM class. The logging level can be adjusted according
+ * to the project's needs. By default, only messages with a level of "LOG_DEBUG"
+ * or above are shown. This can be modified in the platformio.ini file or by
+ * adding one of the following build flags:
+ */
+struct StateLogger {
+    template <class SM, class TEvent>
+    [[gnu::used]] void log_process_event(const TEvent&) {
+        ESP_LOGV(STATE_MACHINE_TAG, "%s", sml::aux::get_type_name<SM>());
+        String eventName = String(sml::aux::get_type_name<TEvent>());
+        // if the event name starts with " boost::ext::sml" then only TRACE it
+        // to reduce verbosity
+        if (eventName.startsWith("boost::ext::sml")) {
+            ESP_LOGV(STATE_MACHINE_TAG, "%s", eventName.c_str());
+        } else {
+            ESP_LOGD(STATE_MACHINE_TAG, "%s", eventName.c_str());
+        }
+    }
+
+    template <class SM, class TGuard, class TEvent>
+    [[gnu::used]] void log_guard(const TGuard&, const TEvent&, bool result) {
+        String resultString = result ? "[PASS]" : "[DO NOT PASS]";
+        ESP_LOGV(STATE_MACHINE_TAG, "%s: %s", resultString,
+                 sml::aux::get_type_name<SM>());
+        ESP_LOGD(STATE_MACHINE_TAG, "%s: %s, %s", resultString,
+                 sml::aux::get_type_name<TGuard>(),
+                 sml::aux::get_type_name<TEvent>());
+    }
+
+    template <class SM, class TAction, class TEvent>
+    [[gnu::used]] void log_action(const TAction&, const TEvent&) {
+        ESP_LOGV(STATE_MACHINE_TAG, "%s", sml::aux::get_type_name<SM>());
+
+        // These are trace messages because lambda functions are very verbose.
+        ESP_LOGV(STATE_MACHINE_TAG, "%s, %s",
+                 sml::aux::get_type_name<TAction>(),
+                 sml::aux::get_type_name<TEvent>());
+    }
+
+    template <class SM, class TSrcState, class TDstState>
+    [[gnu::used]] void log_state_change(const TSrcState& src,
+                                        const TDstState& dst) {
+        ESP_LOGV(STATE_MACHINE_TAG, "%s", sml::aux::get_type_name<SM>());
+        ESP_LOGD(STATE_MACHINE_TAG, "%s -> %s", src.c_str(), dst.c_str());
+    }
+};
+#endif  // OSSM_SOFTWARE_STATELOGGER_H
