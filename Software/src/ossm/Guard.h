ef2ae8da8caba4990d3eaa51f45222c866699dae
AJ <Ajay.Koenig@gmail.com>
2024-05-03 18:36:37 -0600

    revert master to production stable

--- Software/src/ossm/Guard.h
+++
@@ -1,8 +0,0 @@
-#ifndef SOFTWARE_GUARD_H
-#define SOFTWARE_GUARD_H
-
-#include "WiFi.h"
-#include "constants/Menu.h"
-static auto isOnline = []() { return WiFiClass::status() == WL_CONNECTED; };
-
-#endif  // SOFTWARE_GUARD_H
