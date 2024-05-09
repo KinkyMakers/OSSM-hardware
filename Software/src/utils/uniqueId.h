9e499a7792fd9215b39bd0a0b3341323706369b1
AJ <Ajay.Koenig@gmail.com>
2024-05-09 09:17:36 -0600

    temp

--- Software/src/utils/uniqueId.h
+++ Software/src/utils/uniqueId.h
@@ -6,6 +6,27 @@
 
 static UUID uuid;
 
+static String generateId() {
+    // Generate two random seeds using esp_random() function
+    uint32_t seed1 = esp_random();
+    uint32_t seed2 = esp_random();
+
+    // Seed the UUID generator with the generated seeds and set it to variant 4
+    // mode
+    uuid.seed(seed1, seed2);
+    uuid.setVariant4Mode();
+
+    // Generate a new UUID
+    uuid.generate();
+
+    // Convert the generated UUID to a character array and assign it to the id
+    // variable
+    String id = uuid.toCharArray();
+
+    // Return the generated UUID
+    return id;
+}
+
 /**
  * Retrieves a unique identifier (UUID) from device preferences. If a valid UUID
  * is found in the preferences, it returns that UUID. Otherwise, it generates a
@@ -23,21 +44,7 @@ static String getId() {
         return id;
     }
 
-    // Generate two random seeds using esp_random() function
-    uint32_t seed1 = esp_random();
-    uint32_t seed2 = esp_random();
-
-    // Seed the UUID generator with the generated seeds and set it to variant 4
-    // mode
-    uuid.seed(seed1, seed2);
-    uuid.setVariant4Mode();
-
-    // Generate a new UUID
-    uuid.generate();
-
-    // Convert the generated UUID to a character array and assign it to the id
-    // variable
-    id = uuid.toCharArray();
+    id = generateId();
 
     // Save the generated UUID to device preferences for future use
     prefs.putString("uuid", id);
