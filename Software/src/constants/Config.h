229c084a77410c14e20df1d5ab67869cccc7507d
Research and Desire <alexwarner91@gmail.com>
2024-10-23 16:13:44 -0400

    Decreased to 750mm
    This now matches a timeout of 30s and balances flexible use case and convenience for average user.

--- Software/src/constants/Config.h
+++ Software/src/constants/Config.h
@@ -58,7 +58,7 @@ namespace Config {
         // belt attachments subtract the linear block holder length (75mm on
         // OSSM) Recommended to also subtract e.g. 20mm to keep the backstop
         // well away from the device.
-        constexpr float maxStrokeSteps = 1000.0_mm;
+        constexpr float maxStrokeSteps = 750.0_mm;
 
         // If the stroke length is less than this value, then the stroke is
         // likely the result of a poor homing.
