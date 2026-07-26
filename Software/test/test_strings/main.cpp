03697b35d191264b8a9ef9e949d9551f468081e2
AJ <ajay.koenig@gmail.com>
2026-07-27 04:45:30 +0700

    Define user configuration in strings test

--- Software/test/test_strings/main.cpp
+++ Software/test/test_strings/main.cpp
@@ -1,6 +1,10 @@
 #include "unity.h"
 #include "utils/format.h"
 
+namespace UserConfig {
+bool displayMetric = true;
+}
+
 void test_ZeroSeconds(void) {
     TEST_ASSERT_EQUAL_STRING("0s", formatTime(0).c_str());
 }
